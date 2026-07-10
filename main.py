11087a861071dafbe64e1e605068d1a0cb8158bb
Matheus Umpierre <matheus.umpierre@iti.gov.br>
2026-07-10 16:42:43 -0300

    Instala pandoc

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ import json
 import spacy 
 import pypandoc
 from pypandoc.pandoc_download import download_pandoc
-from lxml import etree
 
 output = pypandoc.convert_file(r'C:\Users\matheus.umpierre\Projetos\Ambiente Python\LexML - ICP-Brasil\Resolucao152_revogada.odt', to='html', outputfile='output.html')
-
