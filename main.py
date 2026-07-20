96a655792f2819fc160b67ff5724137c8eda6982
Matheus Umpierre <matheus.umpierre@iti.gov.br>
2026-07-20 13:52:27 -0300

    Adiciona arquivo separado para classes e método NLP para classe AtoNormativo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+from dataclasses import dataclass, field
 import pandas as pd
 import sqlite3
 import pdfplumber
@@ -19,102 +19,7 @@ from pypandoc.pandoc_download import download_pandoc
 #Constantes
 MODELO_NLP = spacy.load('pt_core_news_lg')
 
-#Classes
-class DocumentoURL:
-    '''Lida com documentos hospedados em URL. Tem o arquivo em memória, o texto puro e o doc NLP como propriedades.'''
-    def __init__(self, url: str) -> None:
-        self.url = url
-        self.html = requests.get(url)
-        self.arquivo = io.BytesIO(self.html.content)
-        self.texto_puro = self.__uniformizar_formato()
-        self.doc_nlp = MODELO_NLP(self.texto_puro)
-
-    def __uniformizar_formato(self):
-        if self.url.endswith('.pdf'):
-            self.pdf = pdfplumber.open(self.arquivo)
-            self.numero_paginas = len(self.pdf.pages)
-            self.texto_puro = self.__extrair_texto_pdf(0, self.numero_paginas)
-        elif self.url.endswith(('.html','.htm')):
-            print('Convertendo HTML...')
-            self.texto_puro = BeautifulSoup(self.arquivo, 'lxml').text
-            #self.texto_puro = pypandoc.convert_file(self.arquivo, format='html', to='plain')
-            print('Conversão HTML: Sucesso')
-        else:
-            try:
-                self.texto_puro = BeautifulSoup(self.arquivo, 'html.parser').text
-                print('Conversão HTML: Sucesso')
-            except:
-                self.texto_puro = 'URL não direciona a um elemento .html ou .pdf.'
-        return self.texto_puro
-    
-    def __extrair_texto_pdf(self, pagina_inicial: int = 0, pagina_final: int = 0):
-        self.texto = str()
-        for page in self.pdf.pages[pagina_inicial:pagina_final]:
-            texto_pagina = page.extract_text()
-            self.texto += texto_pagina
-        return self.texto
-    
-class BancoDeDados:
-    '''Lida com direcionamento para o banco de dados'''
-    def __init__(self, arquivo:str) -> None:
-        self.arquivo = arquivo
-        self.db = sqlite3.connect(self.arquivo)
-        pass 
-
-class ConversorHTML():
-    '''Converte um arquivo de texto em um objeto HTML estruturado. Herda métodos das classes ModeloNLP para a inicialização do modelo, '''
-
-    def __init__(self, filepath:str) -> None:
-        self.arquivo = filepath
-        self.parser = HTMLParser
-        self.html_doc = self.doc_to_html()
-        self.convertido = self.parse_html()
-        self.doc = MODELO_NLP(self.convertido.text)
-        self.formatado = self.convertido.prettify()
-        self.paragrafos = [p.get_text() for p in self.convertido.find_all('p')]
-        pass
-
-    def __str__(self):
-        return f'Arquivo localizado em f{self.arquivo}'    
-
-    def doc_to_html(self) -> str:
-        html_doc = pypandoc.convert_file(rf'{self.arquivo}', to='html')
-        return html_doc
-
-    def parse_html(self):
-        soup = BeautifulSoup(self.html_doc, 'html.parser')
-        return soup
-    
-    def localizar_artigos(self):
-        artigos = []
-        for paragrafo in self.paragrafos:
-            if paragrafo.lower().startswith('art.'):
-                artigos.append(paragrafo)
-        return artigos
-
-
-class AtoNormativo_old():
-    '''Usa regras definidas para identificar o tipo de ato normativo (DOC-ICP, Resolução, Instrução Normativa)'''
-    
-    def __init__(self, url) -> None:
-        super().__init__(url)
-            
-        self.regras = {
-        'instrução normativa':'Instrução Normativa',
-        'resolução':'Resolução',
-        'portaria' : 'Portaria'
-        }
-
-        self.rotulo = self.rotular_documento()
-    
-    def rotular_documento(self):
-        for key, value in self.regras.items():
-            if self.doc_nlp[0].text.lower().startswith(key):
-                rotulo = value
-            else:
-                rotulo = 'Desconhecido'
-            return rotulo
-        
+#Classes        
 class AtoNormativo:
     def __init__(self, arquivo:str) -> None:
         self.origem = arquivo
@@ -125,10 +30,14 @@ class AtoNormativo:
         self.titulo: Optional[str] = None
         self.categoria: Optional[str] = None
         self.dispositivos: Optional[list] = None
-        self.texto: Optional[str] = None
+        self.texto: str
 
         self.__obter_conteudo()
         pass
+
+    def processar_nlp(self):
+        self.doc = MODELO_NLP(self.texto)
+        return self.doc
 
     def __obter_conteudo(self): 
         if self.origem.startswith(('http://','https://')):
@@ -137,6 +46,8 @@ class AtoNormativo:
             self.texto = BeautifulSoup(self.arquivo_salvo, 'lxml').text
         elif self.origem.endswith('.pdf'):
             self.__processar_pdf()
+        elif self.origem.endswith(tuple(self.formatos_suportados)):
+            self.__processar_texto_doc()
         else:
             if not self.origem.endswith(tuple(self.formatos_suportados)):
                 print('Formato não suportado')
@@ -149,22 +60,21 @@ class AtoNormativo:
                 self.texto += texto_pagina
         return self.texto
 
-    def __processar_texto(self):
-        pass
+    def __processar_texto_doc(self):
+        html_doc = pypandoc.convert_file(rf'{self.origem}', to='html')
+        self.texto = BeautifulSoup(html_doc, 'lxml').text
+        return self.texto
 
+@dataclass
 class DispositivoNormativo:
-    def __init__(self) -> None:
-        self.prefixo_lex = 'lex:br:instituto.federal.tecnologia.informacao:icp.brasil:'
-        self.atributos_lex:str = ''
-        self.ato_normativo = AtoNormativo
-        self.urn_lex = self.prefixo_lex + self.atributos_lex
-        self.categoria_dispositivo = None
-        pass
+    tipo: str
+    urn: Optional[str]
+    prefixo_lex: str = 'lex:br:instituto.federal.tecnologia.informacao:icp.brasil:'
+    
 
-
-d = AtoNormativo(r'C:\Users\matheus.umpierre\Projetos\lexml_icp_brasil\testes\03_-_RESOLUÇÕES\Site Novo\Resolucao1_revogada.pdf')
+d = AtoNormativo(r'C:\Users\matheus.umpierre\Projetos\lexml_icp_brasil\testes\Resolucao152_revogada.odt')
 
 getter_artigo = lambda span: 'Art' in span.text
 Span.set_extension('is_artigo', getter=getter_artigo)
 
-print(d.texto)
+print(d.processar_nlp())
