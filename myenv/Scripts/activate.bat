98daf3e7305da96e4c149322702d5069ef73bd50
Matheus Umpierre <matheus.umpierre@iti.gov.br>
2026-08-03 16:10:01 -0300

    Commit inicial gitlab

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\matheus.umpierre\Projetos\lexml_icp_brasil\myenv"
+set "VIRTUAL_ENV=C:\Users\matheus.umpierre\Projetos\lexml_icp_brasil_gitlab\lex_icp_brasil\myenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
